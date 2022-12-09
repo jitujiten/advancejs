fd5cb1877ab1c1711841df2180ae5862102ed4b8
jitujiten <sahuohjitu@gmail.com>
2022-12-09 22:41:45 +0530

    Added two files

---
+++ DOM/task3/task3.js
@@ -0,0 +1,8 @@
+var titlename=document.getElementById("header-title");
+var headername=document.getElementById("main-header");
+titlename.innerText="hello";
+titlename.textContent="good-bye";
+headername.style.borderBottom="2px solid black";
+
+var additemname=document.querySelector(".title");
+additemname.style.color="green";
