7082c69faf311f11a55a4e0834ec9f4765dd35b7
jitujiten <sahuohjitu@gmail.com>
2022-12-11 09:19:38 +0530

    Added task6 file

---
+++ DOM/task6/main.js
@@ -0,0 +1,8 @@
+////// Make the 2nd item have green background color
+///// Make the 3rd item invisible
+
+
+var items=document.querySelectorAll(".list-group-item");
+
+items[1].style.backgroundColor="green";
+items[2].style.visibility="hidden";
