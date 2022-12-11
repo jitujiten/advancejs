591250ae7203a1848e5cf67ed929aa59e9e45cb2
jitujiten <sahuohjitu@gmail.com>
2022-12-11 09:08:47 +0530

    Added task5 file

---
+++ DOM/task5/main.js
@@ -0,0 +1,12 @@
+// using classname
+// var items=document.getElementsByClassName("list-group-item");
+// items[4].style.fontWeight="bold";
+
+
+// using tagname
+var li=document.getElementsByTagName("li");
+li[4].style.fontWeight="bold";
+li[4].style.listStyle="none";
+li[4].style.paddingLeft="20px";
+li[4].style.border="solid 0.5px grey";
+
